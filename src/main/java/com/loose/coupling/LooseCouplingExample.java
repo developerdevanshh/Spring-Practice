cac7df5f4e0cad9ff04c914cd75979f86d4ad495
developerdevanshh <vermadev8749@gmail.com>
2026-06-24 18:11:08 +0530

    LooseCouplingExample

--- src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -1,11 +1,14 @@
 package com.loose.coupling;
 
-import com.tight.coupling.UserManager;
-
 public class LooseCouplingExample {
     public static void main(String[] args) {
         UserDataProvider userDataProvider = new UserDatabaseProvider();
-        UserManager userManagerWithDB = new UserManager();
+        UserManager userManagerWithDB = new UserManager(userDataProvider);
         System.out.println(userManagerWithDB.getUserInfo());
+
+        UserDataProvider webServiceDataProvider = new UserDatabaseProvider();
+        UserManager userManagerWithWS = new UserManager(webServiceDataProvider);
+        System.out.println(webServiceDataProvider.getUserDetails());
+
     }
 }
--- src/main/java/com/tight/coupling/UserManager.java
+++ src/main/java/com/tight/coupling/UserManager.java
@@ -1,8 +1,8 @@
-package com.tight.coupling;
-
-public class UserManager {
-    private UserDatabase userDatabase = new UserDatabase();
-    public String getUserInfo(){
-        return userDatabase.getUserDetails();
-    }
-}
+//package com.tight.coupling;
+//
+//public class UserManager {
+//    private UserDatabase userDatabase = new UserDatabase();
+//    public String getUserInfo(){
+//        return userDatabase.getUserDetails();
+//    }
+//}
