506ead67a482d4ede2d20e6657be2bd8acc1cb30
developerdevanshh <vermadev8749@gmail.com>
2026-06-26 18:02:08 +0530

    IOC Example

---
+++ src/main/java/com/ioc/coupling/IOCExample.java
@@ -0,0 +1,19 @@
+package com.ioc.coupling;
+
+import com.loose.coupling.*;
+
+public class IOCExample {
+    public static void main(String[] args) {
+        UserDataProvider userDataProvider = new UserDatabaseProvider();
+        UserManager userManagerWithDB = new UserManager(userDataProvider);
+        System.out.println(userManagerWithDB.getUserInfo());
+
+        UserDataProvider webServiceDataProvider = new WebServiceDataProvider();
+        UserManager userManagerWithWS = new UserManager(webServiceDataProvider);
+        System.out.println(webServiceDataProvider.getUserDetails());
+
+        UserDataProvider newDatabaseProvider = new NewDatabaseProvider();
+        UserManager userManagerWithNewDB = new UserManager(newDatabaseProvider);
+        System.out.println(userManagerWithNewDB.getUserInfo());
+    }
+}
