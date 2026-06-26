54a373f92678de694566d95d6980c04af30b304f
developerdevanshh <vermadev8749@gmail.com>
2026-06-26 15:57:41 +0530

    Constructor Injection Example

--- src/main/java/car/example/constructor/injection/Car.java
+++ src/main/java/car/example/constructor/injection/Car.java
@@ -3,4 +3,7 @@ package car.example.constructor.injection;
 public class Car {
     private Specification specification;
 
+    public void displayDetails(){
+        System.out.println("Car Details: " + specification.toString());
+    }
 }
