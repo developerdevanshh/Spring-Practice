bf292d7e44347a73a450a1e00e7b9ee31bccfc8b
developerdevanshh <vermadev8749@gmail.com>
2026-06-28 19:40:33 +0530

    Autowire Example

---
+++ src/main/java/com/com/example/autowire/type/App.java
@@ -0,0 +1,14 @@
+package com.com.example.autowire.type;
+
+//import com.car.example.bean.MyBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationConstructorInjection.xml");
+
+        Car myCar = (Car) context.getBean("myCar");
+        myCar.displayDetails();
+    }
+}
