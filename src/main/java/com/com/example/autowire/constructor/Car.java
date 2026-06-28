01a8a41454938557bdc5ba2042c13bae5ebcfc1e
developerdevanshh <vermadev8749@gmail.com>
2026-06-28 20:12:40 +0530

    Autowire Example

--- src/main/java/com/com/example/autowire/constructor/Car.java
+++ src/main/java/com/com/example/autowire/constructor/Car.java
@@ -3,10 +3,6 @@ package com.com.example.autowire.constructor;
 public class Car {
     private Specification specification;
 
-    public void setSpecification(Specification specification) {
-        this.specification = specification;
-    }
-
     public void displayDetails(){
         System.out.println("Car Details: " + specification.toString());
     }
