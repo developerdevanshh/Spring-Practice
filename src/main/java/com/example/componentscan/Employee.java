9ee6e6fe1b1197b339801b8ec91fde413d9dc820
developerdevanshh <vermadev8749@gmail.com>
2026-06-29 18:20:37 +0530

    ComponentScan Example

---
+++ src/main/java/com/example/componentscan/Employee.java
@@ -0,0 +1,9 @@
+package com.example.componentscan;
+
+public class Employee {
+    private int employeeId;
+    private String firstName;
+    private String lastName;
+    private double salary;
+
+}
