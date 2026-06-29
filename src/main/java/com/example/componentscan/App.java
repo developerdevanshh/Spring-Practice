d83f7084b60d5ad094dd43395f56e8e0aff94200
developerdevanshh <vermadev8749@gmail.com>
2026-06-29 18:30:51 +0530

    ComponentScan Example

--- src/main/java/com/example/componentscan/App.java
+++ src/main/java/com/example/componentscan/App.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("componentScanDemo");
+        ApplicationContext context = new ClassPathXmlApplicationContext("componentScanDemo.xml");
         Employee employee = context.getBean("employee", Employee.class);
         System.out.println(employee.toString());
     }
